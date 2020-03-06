0e3529d5cb8565a8f5eac31134430c9f246ae8c2
George Blackmore <gtblackmore@gmail.com>
2020-03-06 17:43:52 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,5 +3,8 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
   
   def full_name
-    puts "#{:first_name} #{:last_name}"
+    "#{self.first_name} #{self.last_name}"
+  end
+  
+  
 end
